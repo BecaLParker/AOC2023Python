547447feecb406d2f79380b46a5be08c41dc2d01
BecaLParker <beca.parker@red-gate.com>
2023-12-01 17:06:50 +0000

    WIP part 2

--- Tests/test_main.py
+++ Tests/test_main.py
@@ -1,9 +1,28 @@
 from unittest import TestCase
 
-from main import calibration_value, sum_calibration_values, get_lines_from_file
+from main import calibration_value, sum_calibration_values, get_lines_from_file, first_instance_of_digit_word, \
+    last_instance_of_digit_word
 
 
 class Test(TestCase):
+    def test_first_instance_of_digit_word(self):
+        assert first_instance_of_digit_word('two1nine') == ('two', 0)
+        assert first_instance_of_digit_word('eightwothree') == ('eight', 0)
+        assert first_instance_of_digit_word('abcone2threexyz') == ('one', 3)
+        assert first_instance_of_digit_word('xtwone3four') == ('two', 1)
+        assert first_instance_of_digit_word('4nineeightseven2') == ('nine', 1)
+        assert first_instance_of_digit_word('zoneight234') == ('one', 1)
+        assert first_instance_of_digit_word('7pqrstsixteen') == ('six', 6)
+
+    def test_last_instance_of_digit_word(self):
+        assert last_instance_of_digit_word('two1nine') == ('nine', 7)
+        assert last_instance_of_digit_word('eightwothree') == ('three', 11)
+        assert last_instance_of_digit_word('abcone2threexyz') == ('three', 11)
+        assert last_instance_of_digit_word('xtwone3four') == ('four', 10)
+        assert last_instance_of_digit_word('4nineeightseven2') == ('seven', 14)
+        assert last_instance_of_digit_word('zoneight234') == ('eight', 7)
+        assert last_instance_of_digit_word('7pqrstsixteen') == ('six', 8)
+
     def test_calibration_value(self):
         assert calibration_value('1abc2') == 12
         assert calibration_value('pqr3stu8vwx') == 38
@@ -19,7 +38,12 @@ class Test(TestCase):
 
     def test_sum_calibration_values(self):
         assert sum_calibration_values(['1abc2', 'pqr3stu8vwx', 'a1b2c3d4e5f', 'treb7uchet']) == 142
-        assert sum_calibration_values(['two1nine', 'eightwothree', 'abcone2threexyz', 'xtwone3four', '4nineeightseven2', 'zoneight234', '7pqrstsixteen']) == 281
+        assert sum_calibration_values(
+            ['two1nine', 'eightwothree', 'abcone2threexyz', 'xtwone3four', '4nineeightseven2', 'zoneight234',
+             '7pqrstsixteen']) == 281
 
     def test_get_lines_from_file(self):
         assert get_lines_from_file('./test_input.txt') == ['this_is_a_test\n', 'AND ANOTHER TEST']
+
+
+
--- main.py
+++ main.py
@@ -1,12 +1,67 @@
+def first_instance_of_digit_word(line):
+    digit_words = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
+
+    position = len(line) + 1
+    first_digit_word = ''
+    for digit_word in digit_words:
+        if line.find(digit_word) < position and line.find(digit_word) != -1:
+            position = line.find(digit_word)
+            first_digit_word = digit_word
+    return first_digit_word, position
+
+
+def last_instance_of_digit_word(line):
+    digit_words = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
+
+    end_position = 0
+    last_digit_word = ''
+    for digit_word in digit_words:
+        if line.find(digit_word) + len(digit_word) - 1 > end_position:
+            end_position = line.find(digit_word) + len(digit_word) - 1
+            last_digit_word = digit_word
+    return last_digit_word, end_position
+
+
+def convert_to_digit(string):
+    if string == 'one':
+        return '1'
+    elif string == 'two':
+        return '2'
+    elif string == 'three':
+        return '3'
+    elif string == 'four':
+        return '4'
+    elif string == 'five':
+        return '5'
+    elif string == 'six':
+        return '6'
+    elif string == 'seven':
+        return '7'
+    elif string == 'eight':
+        return '8'
+    elif string == 'nine':
+        return '9'
+
+
 def calibration_value(line):
-    # convert input to string
-    line = str(line)
-    # get the first instance in the string which is a digit
-    first = next(filter(str.isdigit, line))
-    # get the last instance in the string which is a digit
-    last = next(filter(str.isdigit, line[::-1]))
+    # Are there any digits in the line?
+    if any(char.isdigit() for char in line):
+        first_digit = next(filter(str.isdigit, line))
+        first_digit_position = line.find(first_digit)
+        last_digit = next(filter(str.isdigit, line[::-1]))
+        last_digit_position = line.rfind(last_digit)
+        if first_instance_of_digit_word(line)[1] < first_digit_position:
+            first_digit = convert_to_digit(first_instance_of_digit_word(line)[0])
+        if last_instance_of_digit_word(line)[1] > last_digit_position:
+            last_digit = convert_to_digit(last_instance_of_digit_word(line)[0])
+    else:  # There are no digits in the line
+        first_digit = convert_to_digit(first_instance_of_digit_word(line)[0])
+        last_digit = convert_to_digit(last_instance_of_digit_word(line)[0])
+
+    print('first is ' + first_digit + ' and last is ' + last_digit)
+
     # concat the first and last digit and return as int
-    return int(first + last)
+    return int(first_digit + last_digit)
 
 
 def sum_calibration_values(lines):
@@ -28,7 +83,6 @@ def get_lines_from_file(filepath):
     file.close()
     # return the lines
     return lines
-
 
 # print the sum of the calibration values of the lines in the file
 #print(sum_calibration_values(get_lines_from_file('./inputs/day1.txt')))
