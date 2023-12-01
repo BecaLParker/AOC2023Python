dc29155810739097f5bd45271a33d1b881f3ad37
BecaLParker <beca.parker@red-gate.com>
2023-12-01 13:00:31 +0000

    Sum calibration values

--- main.py
+++ main.py
@@ -8,3 +8,13 @@ def calibration_value(line):
     last = next(filter(str.isdigit, line[::-1]))
     # concat the first and last digit and return as int
     return int(first + last)
+
+
+def sum_calibration_values(lines):
+    # initialize sum to 0
+    sum = 0
+    # loop through the lines
+    for line in lines:
+        # add the calibration value of each line to the sum
+        sum += calibration_value(line)
+    return sum
--- test_main.py
+++ test_main.py
@@ -1,6 +1,6 @@
 from unittest import TestCase
 
-from main import calibration_value
+from main import calibration_value, sum_calibration_values
 
 
 class Test(TestCase):
@@ -9,3 +9,6 @@ class Test(TestCase):
         assert calibration_value('pqr3stu8vwx') == 38
         assert calibration_value('a1b2c3d4e5f') == 15
         assert calibration_value('treb7uchet') == 77
+
+    def test_sum_calibration_values(self):
+        assert sum_calibration_values(['1abc2', 'pqr3stu8vwx', 'a1b2c3d4e5f', 'treb7uchet']) == 142
